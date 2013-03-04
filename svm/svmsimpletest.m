8d54afbf71cc18b08f1a7c7841fcab7e248dd171
Sudikoff Lab iMac <imac@dhcp-192-239.cs.dartmouth.edu>
2013-03-04 18:56:07 -0500

    purge

--- svm/svmsimpletest.m
+++
@@ -1,9 +0,0 @@
-%compute distance from boundary
-function [score]=svmsimpletest(x,SVstruct,degree)
-    SVx=SVstruct.SVx;
-    SValpha=SVstruct.SValpha;
-    SVy=SVstruct.SVy;
-    score=SVstruct.bias;
-    for i=1:size(SVx,1)
-        score=score+SValpha(i)*SVy(i)*((SVx(i,:)*x')^degree);
-    end
